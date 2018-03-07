17d560f7f66043fafad0bb9a75d27ce8313e612e
Martin Berthold <martin.berthold@akquinet.de>
2018-03-07 08:09:08 +0100

    Rename Package and Namespace

--- src/QlikQrsHub.cs
+++ src/QlikQrsHub.cs
@@ -7,7 +7,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
  */
 #endregion
 
-namespace Q2gHelperQrsNuget
+namespace Q2gHelperQrs
 {
     #region Usings
     using Newtonsoft.Json;
