1af1be2ed43a3261fd32af328de8cdbcf168b610
Martin Berthold <martin.berthold@akquinet.de>
2018-05-16 10:49:19 +0200

    Add Qrs Objects +semver: feature

--- src/QrsApi.cs
+++ src/QrsApi.cs
@@ -19,39 +19,6 @@
     #endregion
 
     #region Json Communication Classes
-    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
-                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-    public class HubOwer
-    {
-        public Guid? Id { get; set; }
-        public string UserId { get; set; }
-        public string UserDirectory { get; set; }
-        public string Name { get; set; }
-        public List<string> Privileges { get; set; }
-        
-        public override string ToString()
-        {
-            return $"{UserDirectory.ToLowerInvariant()}\\{UserId.ToLowerInvariant()}";
-        }
-    }
-
-    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
-                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-    public class HubReferences
-    {
-        public Guid? Id { get; set; }
-        public string DataLocation { get; set; }
-        public string LogicalPath { get; set; }
-        public string ExternalPath { get; set; }
-        public int ServeOptions { get; set; }
-        public List<string> Privileges { get; set; }
-
-        public override string ToString()
-        {
-            return Id?.ToString();
-        }
-    }
-
     [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore, 
                 NamingStrategyType = typeof(CamelCaseNamingStrategy))]
     public class HubInfo
@@ -61,13 +28,13 @@
         public DateTime? ModifiedDate { get; set; }
         public string ModifiedByUserName { get; set; }
         public List<string> CustomProperties { get; set; }
-        public HubOwer Owner { get; set; }
+        public Owner Owner { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public string Description { get; set; }
         public Uri Uri { get; set; }
         public List<string> Tags { get; set; }
-        public List<HubReferences> References { get; set; }
+        public List<Reference> References { get; set; }
         public List<string> MetaData { get; set; }
         public List<string> Privileges { get; set; }
         public bool? ImpactSecurityAccess { get; set; }
@@ -132,6 +99,276 @@
     public class HubSelectRequest : HubSelectCountRequest
     {
         public string OrderBy { get; set; }
+    }
+    #endregion
+
+    #region Qlik Qrs Objects
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+               NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class QlikAnalyticConnection
+    {
+        public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedByUserName { get; set; }
+        public List<object> CustomProperties { get; set; }
+        public string Name { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string CertificateFilePath { get; set; }
+        public int ReconnectTimeout { get; set; }
+        public int RequestTimeout { get; set; }
+        public List<string> Privileges { get; set; }
+        public string SchemaPath { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class QlikSecurityRule
+    {
+        public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedByUserName { get; set; }
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Rule { get; set; }
+        public string ResourceFilter { get; set; }
+        public int Actions { get; set; }
+        public string Comment { get; set; }
+        public bool Disabled { get; set; }
+        public int RuleContext { get; set; }
+        public string SeedId { get; set; }
+        public int Version { get; set; }
+        public List<object> Tags { get; set; }
+        public object Privileges { get; set; }
+        public string SchemaPath { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class LogVerbosity
+    {
+        public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedByUserName { get; set; }
+        public int LogVerbosityAuditActivity { get; set; }
+        public int LogVerbosityAuditSecurity { get; set; }
+        public int LogVerbosityService { get; set; }
+        public int LogVerbosityAudit { get; set; }
+        public int LogVerbosityPerformance { get; set; }
+        public int LogVerbositySecurity { get; set; }
+        public int LogVerbositySystem { get; set; }
+        public string SchemaPath { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class Role
+    {
+        public string Id { get; set; }
+        public int Definition { get; set; }
+        public List<string> Privileges { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class ServiceCluster
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Privileges { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class LoadBalancingServerNode
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string HostName { get; set; }
+        public string Temporaryfilepath { get; set; }
+        public List<Role> Roles { get; set; }
+        public ServiceCluster ServiceCluster { get; set; }
+        public List<string> Privileges { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class VirtualProxy
+    {
+        public string Id { get; set; }
+        public string Prefix { get; set; }
+        public string Description { get; set; }
+        public string AuthenticationModuleRedirectUri { get; set; }
+        public string SessionModuleBaseUri { get; set; }
+        public string LoadBalancingModuleBaseUri { get; set; }
+        public bool UseStickyLoadBalancing { get; set; }
+        public List<LoadBalancingServerNode> LoadBalancingServerNodes { get; set; }
+        public int AuthenticationMethod { get; set; }
+        public int HeaderAuthenticationMode { get; set; }
+        public string HeaderAuthenticationHeaderName { get; set; }
+        public string HeaderAuthenticationStaticUserDirectory { get; set; }
+        public string HeaderAuthenticationDynamicUserDirectory { get; set; }
+        public int AnonymousAccessMode { get; set; }
+        public string WindowsAuthenticationEnabledDevicePattern { get; set; }
+        public string SessionCookieHeaderName { get; set; }
+        public string SessionCookieDomain { get; set; }
+        public string AdditionalResponseHeaders { get; set; }
+        public int SessionInactivityTimeout { get; set; }
+        public bool ExtendedSecurityEnvironment { get; set; }
+        public List<string> WebsocketCrossOriginWhiteList { get; set; }
+        public bool DefaultVirtualProxy { get; set; }
+        public List<object> Tags { get; set; }
+        public string SamlMetadataIdP { get; set; }
+        public string SamlHostUri { get; set; }
+        public string SamlEntityId { get; set; }
+        public string SamlAttributeUserId { get; set; }
+        public string SamlAttributeUserDirectory { get; set; }
+        public int SamlAttributeSigningAlgorithm { get; set; }
+        public List<object> SamlAttributeMap { get; set; }
+        public string JwtAttributeUserId { get; set; }
+        public string JwtAttributeUserDirectory { get; set; }
+        public string JwtPublicKeyCertificate { get; set; }
+        public List<object> JwtAttributeMap { get; set; }
+        public string MagicLinkHostUri { get; set; }
+        public string MagicLinkFriendlyName { get; set; }
+        public bool SamlSlo { get; set; }
+        public List<string> Privileges { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class Settings
+    {
+        public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedByUserName { get; set; }
+        public int ListenPort { get; set; }
+        public bool AllowHttp { get; set; }
+        public int UnencryptedListenPort { get; set; }
+        public int AuthenticationListenPort { get; set; }
+        public bool KerberosAuthentication { get; set; }
+        public int UnencryptedAuthenticationListenPort { get; set; }
+        public string SslBrowserCertificateThumbprint { get; set; }
+        public int KeepAliveTimeoutSeconds { get; set; }
+        public int MaxHeaderSizeBytes { get; set; }
+        public int MaxHeaderLines { get; set; }
+        public LogVerbosity LogVerbosity { get; set; }
+        public bool UseWsTrace { get; set; }
+        public int PerformanceLoggingInterval { get; set; }
+        public int RestListenPort { get; set; }
+        public List<VirtualProxy> VirtualProxies { get; set; }
+        public string FormAuthenticationPageTemplate { get; set; }
+        public object LoggedOutPageTemplate { get; set; }
+        public string ErrorPageTemplate { get; set; }
+        public string SchemaPath { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class ServerNodeConfiguration
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string HostName { get; set; }
+        public string Temporaryfilepath { get; set; }
+        public List<Role> Roles { get; set; }
+        public ServiceCluster ServiceCluster { get; set; }
+        public List<string> Privileges { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                 NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class QlikVirtualProxySettings
+    {
+        public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedByUserName { get; set; }
+        public List<object> CustomProperties { get; set; }
+        public Settings Settings { get; set; }
+        public ServerNodeConfiguration ServerNodeConfiguration { get; set; }
+        public List<object> Tags { get; set; }
+        public List<string> Privileges { get; set; }
+        public string SchemaPath { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class QlikVersion
+    {
+        public string BuildVersion { get; set; }
+        public string BuildDate { get; set; }
+        public string DatabaseProvider { get; set; }
+        public int NodeType { get; set; }
+        public bool SharedPersistence { get; set; }
+        public bool RequiresBootstrap { get; set; }
+        public bool SingleNodeOnly { get; set; }
+        public string SchemaPath { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class Owner
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string UserDirectory { get; set; }
+        public string Name { get; set; }
+        public List<string> Privileges { get; set; }
+
+        public override string ToString()
+        {
+            return $"{UserDirectory.ToLowerInvariant()}\\{UserId.ToLowerInvariant()}";
+        }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                 NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class WhiteList
+    {
+        public string Id { get; set; }
+        public int LibraryType { get; set; }
+        public List<string> Privileges { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class Reference
+    {
+        public string Id { get; set; }
+        public string DataLocation { get; set; }
+        public string LogicalPath { get; set; }
+        public string ExternalPath { get; set; }
+        public int ServeOptions { get; set; }
+        public List<string> Privileges { get; set; }
+
+        public override string ToString()
+        {
+            return Id?.ToString();
+        }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore,
+                NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class QlikExtention
+    {
+        public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedByUserName { get; set; }
+        public List<object> CustomProperties { get; set; }
+        public Owner Owner { get; set; }
+        public string Name { get; set; }
+        public List<object> Tags { get; set; }
+        public WhiteList WhiteList { get; set; }
+        public List<Reference> References { get; set; }
+        public List<string> Privileges { get; set; }
+        public string SchemaPath { get; set; }
     }
     #endregion
 }
